bfbdcf9d5a6c70a85c71f74ca99ed0631b8b00c2
Olga Vozchikova <vozchikova@gmail.com>
2025-11-12 16:05:29 +0000

    small fixes

--- index.js
+++ index.js
@@ -105,5 +105,13 @@ function setWrapperX(posX) {
     WRAPPER.style.transform = `translateX(${posX}px)`;
 }
 
-// Init
 initCarousel();
+
+const menuToggle = document.getElementById('menu-toggle');
+const menuLinks = document.querySelectorAll('.menu-link');
+
+menuLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+        menuToggle.checked = false;
+    });
+});
